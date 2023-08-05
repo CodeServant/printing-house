e76bd9e9ee51c2818333388525724773a10395d3
CodeServant <49171565+CodeServant@users.noreply.github.com>
2023-08-05 22:10:40 +0200

    init git repo with

---
+++ printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/PrintingHouseServerApplication.kt
@@ -0,0 +1,11 @@
+package pl.macia.printinghouse.server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrintingHouseServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrintingHouseServerApplication>(*args)
+}
