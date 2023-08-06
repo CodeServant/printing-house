cde8c5e33df06fcb499f3b65599db08bce0cade0
CodeServant <49171565+CodeServant@users.noreply.github.com>
2023-08-06 17:44:12 +0200

    add gradle dependencies for spring-boot-starter-data-jpa, poko-annotations, spring-boot-starter-validation

--- printing-house-server/build.gradle.kts
+++ printing-house-server/build.gradle.kts
@@ -24,6 +24,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("dev.drewhamilton.poko:poko-annotations:0.14.0")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 tasks.withType<KotlinCompile> {
