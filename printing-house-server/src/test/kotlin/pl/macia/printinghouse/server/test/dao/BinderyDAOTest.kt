73515aeed7931361ac00461d76a1151ba485169c
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-08-08 11:51:00 +0200

    added BinderyDAOTest test for creating Bindery

--- printing-house-server/src/test/kotlin/pl/macia/printinghouse/server/test/dao/BinderyDAOTest.kt
+++ printing-house-server/src/test/kotlin/pl/macia/printinghouse/server/test/dao/BinderyDAOTest.kt
@@ -1,14 +1,14 @@
 package pl.macia.printinghouse.server.test.dao
 
+import jakarta.validation.ConstraintViolationException
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.MethodOrderer
-import org.junit.jupiter.api.Order
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.dao.DataIntegrityViolationException
 import pl.macia.printinghouse.server.PrintingHouseServerApplication
 import pl.macia.printinghouse.server.dao.BinderyDAO
+import pl.macia.printinghouse.server.dto.Bindery
 
 @SpringBootTest(classes = [PrintingHouseServerApplication::class])
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
@@ -35,5 +35,30 @@ class BinderyDAOTest {
         assertNotNull(bindery, "bindery $searchedName not found")
         val expectedId = 1
         assertEquals(1, bindery?.id, "expected id is $expectedId")
+    }
+
+    @Test
+    fun `test create new`() {
+        var bindery = Bindery("testCreateNew")
+        dao.create(bindery)
+        assertNotNull(bindery.id)
+        bindery = Bindery("testCreateNew")
+        assertThrows<DataIntegrityViolationException>("data cannot be duplicated") {
+            dao.create(bindery)
+        }
+        val maxLength = 200
+        val binderyMetaName = "bindery"
+        bindery = Bindery("".padEnd(maxLength, 'a'))
+        assertDoesNotThrow("$binderyMetaName name should include $maxLength characters") {
+            dao.create(bindery)
+        }
+        bindery = Bindery("".padEnd(maxLength + 1, 'b'))
+        assertThrows<ConstraintViolationException>("$binderyMetaName max length is $maxLength") {
+            dao.create(bindery)
+        }
+        bindery = Bindery(" ")
+        assertThrows<ConstraintViolationException> {
+            dao.create(bindery)
+        }
     }
 }
