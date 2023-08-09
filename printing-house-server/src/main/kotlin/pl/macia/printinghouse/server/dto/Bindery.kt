62543db7499f23993f033504be7bd72bbdf433a0
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-08-09 12:17:56 +0200

    defining constatnts to data data models to resolve names at compile time

--- printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/dto/Bindery.kt
+++ printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/dto/Bindery.kt
@@ -11,23 +11,27 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
+const val tableBindery = "Bindery"
+const val binderyId = "id"
+const val binderyName = "name"
+
 @Poko
 @Entity
-@Table(name = "Bindery")
+@Table(name = tableBindery)
 class Bindery(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = binderyId)
     var id: Int?,
     @field:NotNull
     @field:NotBlank
     @field:Size(max = 200)
-    @Column(name = "name")
+    @Column(name = binderyName)
     var name: String?
 ) {
     constructor(name: String) : this(null, name)
 
     override fun toString(): String {
-        return "{$id, $name}"
+        return "{$binderyId: $id, $binderyName: $name}"
     }
 }
--- printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/dto/Person.kt
+++ printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/dto/Person.kt
@@ -6,30 +6,36 @@ import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
+const val tablePerson = "Person"
+const val personId = "id"
+const val personName = "name"
+const val personSurname = "surname"
+const val personPseudoPESEL = "pseudoPESEL"
+
 @Poko
 @Entity
-@Table(name = "Person")
+@Table(name = tablePerson)
 open class Person(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = personId)
     val id: Int?,
     @field:NotNull
     @field:NotBlank
     @field:Size(max = 200)
-    @Column(name = "name")
+    @Column(name = personName)
     val name: String,
     @field:NotNull
     @field:NotBlank
     @field:Size(max = 300)
-    @Column(name = "surname")
+    @Column(name = personSurname)
     val surname: String,
     @field:NotNull
     @field:Size(min = 11, max = 11)
-    @Column(name = "pseudoPESEL")
+    @Column(name = personPseudoPESEL)
     val pseudoPESEL: String
 ) {
     override fun toString(): String {
-        return "{id: $id, name: $name, surname: $surname, pseudoPESEL: $pseudoPESEL}"
+        return "{$personId: $id, $personName: $name, $personSurname: $surname, $personPseudoPESEL: $pseudoPESEL}"
     }
 }
