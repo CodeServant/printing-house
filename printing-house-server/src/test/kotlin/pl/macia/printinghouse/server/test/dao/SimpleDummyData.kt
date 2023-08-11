52a6651edfc7e2afb1535b280966ce0aa251d808
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-08-11 13:53:38 +0200

    change the name of parameter for generateLongName

--- printing-house-server/src/test/kotlin/pl/macia/printinghouse/server/test/dao/SimpleDummyData.kt
+++ printing-house-server/src/test/kotlin/pl/macia/printinghouse/server/test/dao/SimpleDummyData.kt
@@ -1,9 +1,9 @@
 package pl.macia.printinghouse.server.test.dao
 
 /**
- * Generate text value with exact length repeating string of [padd]. Last string can be cropped.
+ * Generate text value with exact length repeating string of [pad]. Last string can be cropped.
  */
-fun generateLongName(size: Int, padd: String = "a"):String {
-    val times = size/padd.length+1
-    return padd.repeat(times).subSequence(0, size).toString()
+fun generateLongName(size: Int, pad: String = "a"):String {
+    val times = size/pad.length+1
+    return pad.repeat(times).subSequence(0, size).toString()
 }
