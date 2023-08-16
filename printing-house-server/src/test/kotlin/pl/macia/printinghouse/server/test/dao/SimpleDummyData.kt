d964299abaeda8f678310c48ecafb63b3019b9e1
CodeServant <49171565+CodeServant@users.noreply.github.com>
2023-08-16 19:51:29 +0200

    inline some code in SimpleDummyData.kt

--- printing-house-server/src/test/kotlin/pl/macia/printinghouse/server/test/dao/SimpleDummyData.kt
+++ printing-house-server/src/test/kotlin/pl/macia/printinghouse/server/test/dao/SimpleDummyData.kt
@@ -3,7 +3,7 @@ package pl.macia.printinghouse.server.test.dao
 /**
  * Generate text value with exact length repeating string of [pad]. Last string can be cropped.
  */
-fun generateLongName(size: Int, pad: String = "a"):String {
-    val times = size/pad.length+1
+fun generateLongName(size: Int, pad: String = "a"): String {
+    val times = size / pad.length + 1
     return pad.repeat(times).subSequence(0, size).toString()
 }
