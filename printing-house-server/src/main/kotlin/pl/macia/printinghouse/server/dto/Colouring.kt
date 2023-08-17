faedffed507ea2521101afc00c66504d0fc78ecb
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-08-17 15:31:28 +0200

    add Colouring entity, ColouringDAO, ColouringDAOCustom, ColouringDAOTest

---
+++ printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/dto/Colouring.kt
@@ -0,0 +1,40 @@
+package pl.macia.printinghouse.server.dto
+
+import dev.drewhamilton.poko.Poko
+import jakarta.persistence.*
+import jakarta.validation.constraints.PositiveOrZero
+import org.hibernate.validator.constraints.Range
+
+const val tableColouring = "Colouring"
+const val colouringId = "id"
+const val colouringFirstSide = "firstSide"
+const val colouringSecondSide = "secondSide"
+
+@Poko
+@Entity
+@Table(name = tableColouring)
+final class Colouring(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = colouringId)
+    var id: Byte?,
+    @Column(name = colouringFirstSide)
+    @field:PositiveOrZero
+    @field:Range(max = 4)
+    var firstSide: Byte,
+    @Column(name = colouringSecondSide)
+    @field:PositiveOrZero
+    @field:Range(max = 4)
+    var secondSide: Byte
+) {
+    init {
+        if (this.firstSide < this.secondSide) {
+            val tmp = this.firstSide
+            this.firstSide = this.secondSide
+            this.secondSide = tmp
+        }
+    }
+
+    constructor(firstSide: Byte, secondSide: Byte) : this(null, firstSide, secondSide)
+
+}
---
+++ printing-house-server/src/test/kotlin/pl/macia/printinghouse/server/test/dao/ColouringDAOTest.kt
@@ -0,0 +1,68 @@
+package pl.macia.printinghouse.server.test.dao
+
+import jakarta.transaction.Transactional
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+import org.junit.jupiter.api.assertThrows
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.data.repository.findByIdOrNull
+import pl.macia.printinghouse.server.PrintingHouseServerApplication
+import pl.macia.printinghouse.server.dao.ColouringDAO
+import pl.macia.printinghouse.server.dto.Colouring
+
+@SpringBootTest(classes = [PrintingHouseServerApplication::class])
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class ColouringDAOTest {
+    @Autowired
+    lateinit var dao: ColouringDAO
+
+    @Test
+    fun `find by id`() {
+        assertEquals(0, dao.findByIdOrNull(2)?.secondSide)
+        assertNull(dao.findByIdOrNull(10)?.secondSide)
+    }
+
+    @Test
+    @Transactional
+    fun `create one test`() {
+        var inserted: Colouring? = null
+        assertDoesNotThrow {
+            inserted = dao.save(Colouring(3, 2))
+        }
+        assertNotNull(inserted?.id)
+        assertEquals(3, inserted?.firstSide)
+    }
+
+    @Test
+    fun `unique validation test`() {
+        assertThrows<DataIntegrityViolationException> {
+            dao.save(Colouring(1, 1))
+        }
+    }
+
+    @Test
+    fun `foreign key constraint test`() {
+        assertThrows<DataIntegrityViolationException> {
+            dao.deleteById(4)
+        }
+    }
+
+    @Test
+    @Transactional
+    fun `delete one test`() {
+        dao.deleteById(1)
+        assertEquals(3, dao.count())
+    }
+
+    @Test
+    @Transactional
+    fun `first side higher test`() {
+        assertThrows<DataIntegrityViolationException> {
+            dao.save(Colouring(0,4))
+        }
+    }
+}
