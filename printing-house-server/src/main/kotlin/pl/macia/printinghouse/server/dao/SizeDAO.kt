ee0200358da7787d2ff5db10f45a84632713c0cb
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-08-21 14:09:22 +0200

    added findByName to SizeDAO interface for Spring Boot to initialize

--- printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/dao/SizeDAO.kt
+++ printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/dao/SizeDAO.kt
@@ -3,4 +3,6 @@ package pl.macia.printinghouse.server.dao
 import org.springframework.data.jpa.repository.JpaRepository
 import pl.macia.printinghouse.server.dto.Size
 
-interface SizeDAO : JpaRepository<Size, Int>, SizeDAOCustom
+interface SizeDAO : JpaRepository<Size, Int>, SizeDAOCustom {
+    fun findByName(s: String): Size?
+}
