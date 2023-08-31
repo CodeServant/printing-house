191b126512297c4d13413bdd23f267b552201b40
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-08-31 16:31:29 +0200

    add h2 as test dependency in repo module

--- printing-house-server/repo/build.gradle.kts
+++ printing-house-server/repo/build.gradle.kts
@@ -15,10 +15,12 @@ dependencies {
     val mysqlVersion = project.properties["mysqlVersion"].toString()
     val pokoVersion = project.properties["pokoVersion"].toString()
     val springVersion = project.properties["springBootVersion"].toString()
+    val h2db = project.properties["h2db"].toString()
     runtimeOnly("com.mysql:mysql-connector-j:$mysqlVersion")
     implementation("dev.drewhamilton.poko:poko-annotations:$pokoVersion")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:$springVersion")
     implementation("org.springframework.boot:spring-boot-starter-validation:$springVersion")
+    testImplementation("com.h2database:h2:$h2db")
 }
 
 allOpen {
