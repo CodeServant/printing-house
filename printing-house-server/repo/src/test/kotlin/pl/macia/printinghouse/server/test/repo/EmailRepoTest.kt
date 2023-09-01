3ece6853490a6b6e922060f55933c155ad583b0c
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-01 10:14:57 +0200

    ContextConfiguration to TestPropertySource to the in memory tests

--- printing-house-server/repo/src/test/kotlin/pl/macia/printinghouse/server/test/repo/EmailRepoTest.kt
+++ printing-house-server/repo/src/test/kotlin/pl/macia/printinghouse/server/test/repo/EmailRepoTest.kt
@@ -1,11 +1,10 @@
 package pl.macia.printinghouse.server.test.repo
 
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
 import pl.macia.printinghouse.server.PrintingHouseServerApplication
-import pl.macia.printinghouse.server.test.InMemProfile
 @SpringBootTest(classes = [PrintingHouseServerApplication::class])
-@ContextConfiguration(classes = [InMemProfile::class])
+@TestPropertySource("classpath:inMemDB.properties")
 internal class EmailRepoTest {
 
 }
