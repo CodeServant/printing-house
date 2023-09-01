59e1993888eb2d986da2e5f189b0d56c70388c60
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-01 12:48:16 +0200

    create marker interfaces for Repos in BaseRepo.kt

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/repository/BaseRepo.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/repository/BaseRepo.kt
@@ -1,6 +1,12 @@
 package pl.macia.printinghouse.server.repository
 
+import pl.macia.printinghouse.server.bmodel.Email
+import pl.macia.printinghouse.server.bmodel.IndividualClient
+
 interface BaseRepo<T, ID> {
     fun findById(id: ID): T?
     fun save(obj: T): T
 }
+
+interface EmailRepo : BaseRepo<Email, Int>
+interface IndividualClientRepo : BaseRepo<IndividualClient, Int>
