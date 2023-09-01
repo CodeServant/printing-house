6b53e30e217c023f6d9410977a1e903062b94f1c
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-01 12:57:05 +0200

    create IndividualClientImpl

---
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/IndividualClientImpl.kt
@@ -0,0 +1,55 @@
+package pl.macia.printinghouse.server.bmodel
+
+import pl.macia.printinghouse.server.dto.Client
+import pl.macia.printinghouse.server.dto.IndividualClient as PInCli
+
+internal class IndividualClientImpl(persistent: PInCli) : IndividualClient,
+    ConvertableAbstract<PInCli>(persistent) {
+
+    constructor(
+        phoneNumber: String?,
+        email: EmailImpl?,
+        psudoPESEL: String,
+        surname: String,
+        name: String
+    ) : this(
+        PInCli(
+            name = name,
+            surname = surname,
+            pseudoPESEL = psudoPESEL,
+            client = Client(
+                email = email?.persistent,
+                phoneNumber = phoneNumber
+            )
+        )
+    )
+
+    override var clientId: Int? by persistent.client::id
+    override var phoneNumber: String? by persistent.client::phoneNumber
+    override var email: Email? by delegate(persistent.client.email, ::EmailImpl, Email::class.java)
+    override var personId: Int? by persistent::id
+    override var psudoPESEL: String by persistent::pseudoPESEL
+    override var surname: String by persistent::surname
+    override var name: String by persistent::name
+}
+
+fun IndividualClient(
+    phoneNumber: String?,
+    email: Email?,
+    psudoPESEL: String,
+    surname: String,
+    name: String
+): IndividualClient {
+    email as EmailImpl
+    return IndividualClientImpl(
+        PInCli(
+            name = name,
+            surname = surname,
+            pseudoPESEL = psudoPESEL,
+            client = Client(
+                email = email.persistent,
+                phoneNumber = phoneNumber
+            )
+        )
+    )
+}
