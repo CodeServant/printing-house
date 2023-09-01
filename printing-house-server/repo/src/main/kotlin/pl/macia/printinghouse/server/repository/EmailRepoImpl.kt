840a7b7d2ec54fb227a964d58914b301d6bfbe4d
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-01 12:51:12 +0200

    create EmailRepoImpl and EmailRepoTest

---
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/repository/EmailRepoImpl.kt
@@ -0,0 +1,23 @@
+package pl.macia.printinghouse.server.repository
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
+import pl.macia.printinghouse.server.bmodel.Email
+import pl.macia.printinghouse.server.bmodel.EmailImpl
+import pl.macia.printinghouse.server.dao.EmailDAO
+
+@Repository
+class EmailRepoImpl : EmailRepo {
+    @Autowired
+    private lateinit var dao: EmailDAO
+
+    override fun findById(id: Int): Email? {
+        return dao.findByIdOrNull(id)?.let { EmailImpl(it) }
+    }
+
+    override fun save(email: Email): Email {
+        email as EmailImpl
+        return dao.save(email.persistent).let { EmailImpl(it) }
+    }
+}
--- printing-house-server/repo/src/test/kotlin/pl/macia/printinghouse/server/test/repo/EmailRepoTest.kt
+++ printing-house-server/repo/src/test/kotlin/pl/macia/printinghouse/server/test/repo/EmailRepoTest.kt
@@ -1,10 +1,21 @@
 package pl.macia.printinghouse.server.test.repo
 
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestPropertySource
 import pl.macia.printinghouse.server.PrintingHouseServerApplication
+import pl.macia.printinghouse.server.repository.EmailRepo
+
 @SpringBootTest(classes = [PrintingHouseServerApplication::class])
 @TestPropertySource("classpath:inMemDB.properties")
 internal class EmailRepoTest {
+    @Autowired
+    lateinit var dao: EmailRepo
 
+    @Test
+    fun name() {
+        assertEquals("anna@wp.pl", dao.findById(1)?.email)
+    }
 }
