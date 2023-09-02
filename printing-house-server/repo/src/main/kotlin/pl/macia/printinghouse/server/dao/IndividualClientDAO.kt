027b9fe94eee76504307ac7b1743996b2eacbd2f
CodeServant <49171565+CodeServant@users.noreply.github.com>
2023-09-02 22:52:50 +0200

    expand IndividualClientDAO to findByClientId

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/dao/IndividualClientDAO.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/dao/IndividualClientDAO.kt
@@ -3,4 +3,7 @@ package pl.macia.printinghouse.server.dao
 import org.springframework.data.jpa.repository.JpaRepository
 import pl.macia.printinghouse.server.dto.IndividualClient
 
-internal interface IndividualClientDAO : JpaRepository<IndividualClient, Int>, IndividualClientDAOCustom
+internal interface IndividualClientDAO : JpaRepository<IndividualClient, Int>, IndividualClientDAOCustom{
+
+    fun findByClientId(clientId: Int): IndividualClient?
+}
