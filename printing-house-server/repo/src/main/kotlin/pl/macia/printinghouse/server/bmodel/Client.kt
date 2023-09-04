1977409f00f83e0d5d7676d37e114c45c05accda
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-04 16:57:14 +0200

    dokka annotation about casting Clients subtypes

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/Client.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/Client.kt
@@ -1,5 +1,8 @@
 package pl.macia.printinghouse.server.bmodel
 
+/**
+ * Keep in mind that you can't rely on casting or type checking with the [Client] because that would be resource consuming by database.
+ */
 sealed interface Client {
     var clientId: Int?
     var phoneNumber: String?
