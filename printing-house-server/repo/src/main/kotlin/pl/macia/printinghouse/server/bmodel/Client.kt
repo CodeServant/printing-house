f9a18fb1acbfaa73dd310507ecdf301742db9f6d
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-05 11:15:55 +0200

    dokka annotation remove dokka annotation about casting

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/Client.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/Client.kt
@@ -1,8 +1,5 @@
 package pl.macia.printinghouse.server.bmodel
 
-/**
- * Keep in mind that you can't rely on casting or type checking with the [Client] because that would be resource consuming by database.
- */
 sealed interface Client {
     var clientId: Int?
     var phoneNumber: String?
