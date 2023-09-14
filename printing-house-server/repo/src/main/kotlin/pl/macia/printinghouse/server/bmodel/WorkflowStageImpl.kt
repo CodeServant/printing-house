d33a360b97eacb67bf6b75add42dd980ee878033
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-14 15:22:19 +0200

    create function constructor for WorkflowStageimpl

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/WorkflowStageImpl.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/WorkflowStageImpl.kt
@@ -6,6 +6,12 @@ import pl.macia.printinghouse.server.dto.Worker as PWorker
 internal class WorkflowStageImpl(
     persistent: PWorkflowStage,
 ) : WorkflowStage, BusinessBase<PWorkflowStage>(persistent) {
+    constructor(
+        role: RoleImpl,
+        name: String
+    ) : this(
+        PWorkflowStage(role.persistent, name, mutableListOf())
+    )
     override var workflowStageid: Int? by persistent::id
     override var name: String by persistent::name
     override val workflowManagers: BMutableList<Worker, PWorker> =
@@ -14,4 +20,8 @@ internal class WorkflowStageImpl(
             it.persistent
         }, persistent.workflowStageManagers)
     override var role: Role by delegate(persistent.role, ::RoleImpl, Role::class.java)
+}
+
+fun WorkflowStage(role: Role, name: String): WorkflowStage {
+    return WorkflowStageImpl(role as RoleImpl, name)
 }
