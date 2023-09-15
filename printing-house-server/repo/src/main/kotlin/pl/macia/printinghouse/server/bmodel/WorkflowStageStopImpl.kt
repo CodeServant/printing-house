25459e20bddc7fa265bb20c5727d66f8b0cac7d1
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-15 15:09:23 +0200

    WorkflowStageStopImpl without order association

---
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/WorkflowStageStopImpl.kt
@@ -0,0 +1,22 @@
+package pl.macia.printinghouse.server.bmodel
+
+import java.time.LocalDateTime
+import pl.macia.printinghouse.server.dto.WorkflowStageStop as PWorkflowStageStop
+
+internal class WorkflowStageStopImpl(p: PWorkflowStageStop) : WorkflowStageStop, BusinessBase<PWorkflowStageStop>(p) {
+    //TODO constructor
+    override var wfssId: Int? by persistent::id
+    override var comment: String? by persistent::comment
+    override var lastWorkflowStage: Boolean by persistent::lastWorkflowStage
+    override var assignTime: LocalDateTime? by persistent::assignTime
+    override var createTime: LocalDateTime by persistent::createTime
+    override var worker: Worker? by delegate(persistent.worker, ::WorkerImpl, Worker::class.java)
+    override var workflowStage: WorkflowStage by delegate(
+        persistent.workflowStage,
+        ::WorkflowStageImpl,
+        WorkflowStage::class.java
+    )
+    override var order: Order
+        get() = TODO("Not yet implemented")
+        set(value) {}
+}
