c315d1b7d1f601573a3cb92f96765fa535cf0aac
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-15 13:49:54 +0200

    clarification to documentation

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/WorkflowStage.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/WorkflowStage.kt
@@ -1,5 +1,8 @@
 package pl.macia.printinghouse.server.bmodel
 
+/**
+ * Since someone have to conduct [WorkflowStage] there have to be at least one [Worker] in [workflowManagers].
+ */
 sealed interface WorkflowStage {
     var workflowStageid: Int?
     var name: String
