570c60f1ada5595faead472deb947dd0d8367f68
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-20 12:24:33 +0200

    create new order test

--- printing-house-server/repo/src/test/kotlin/pl/macia/printinghouse/server/test/repo/OrderRepoTest.kt
+++ printing-house-server/repo/src/test/kotlin/pl/macia/printinghouse/server/test/repo/OrderRepoTest.kt
@@ -1,18 +1,36 @@
 package pl.macia.printinghouse.server.test.repo
 
+import jakarta.transaction.Transactional
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestPropertySource
 import pl.macia.printinghouse.server.PrintingHouseServerApplication
-import pl.macia.printinghouse.server.repository.OrderRepo
+import pl.macia.printinghouse.server.bmodel.*
+import pl.macia.printinghouse.server.repository.*
+import java.time.LocalDateTime
 
 @SpringBootTest(classes = [PrintingHouseServerApplication::class])
 @TestPropertySource("classpath:inMemDB.properties")
 internal class OrderRepoTest {
     @Autowired
-    lateinit var repo: OrderRepo
+    lateinit var repo: OrderIntRepo
+
+    @Autowired
+    lateinit var clientRepo: ClientIntRepo
+
+    @Autowired
+    lateinit var binderyRepo: BinderyIntRepo
+
+    @Autowired
+    lateinit var salesmanRepo: SalesmanIntRepo
+
+    @Autowired
+    lateinit var bindingFormRepo: BindingFormIntRepo
+
+    @Autowired
+    lateinit var workflowStageRepo: WorkflowStageIntRepo
 
     @Test
     fun `find by id test`() {
@@ -36,5 +54,89 @@ internal class OrderRepoTest {
         assertNull(found.completionDate)
         assertNull(found.withdrawalDate)
         assertNull(found.comment)
+    }
+
+    @Test
+    @Transactional
+    fun `create test`() {
+        val new = Order(
+            name = "someNameCreateTest",
+            netSize = Size(100, 100), comment = "commentSome",
+            withdrawalDate = LocalDateTime.now(),
+            completionDate = LocalDateTime.now(),
+            designsNumberForSheet = 4,
+            checked = true,
+            imageComment = null,
+            towerCut = true,
+            folding = true,
+            clientRepo.findById(1)!!,
+            realizationDate = LocalDateTime.now(),
+            caretionDate = LocalDateTime.now(),
+            3,
+            null,
+            binderyRepo.findById(1)!!,
+            salesmanRepo.findById(1)!!,
+            bindingFormRepo.findById(1)!!
+        )
+        new.addOrderEnobling(
+            null,
+            UVVarnish("addOrderEnobling", null),
+            Bindery("A1")
+        )
+        new.addPaperOrderType(
+            grammage = 10,
+            stockCirculation = 1,
+            sheetNumber = 1,
+            comment = null,
+            circulation = 1,
+            platesQuantityForPrinter = 1,
+            PaperType("Papier Błysk", "Pap Błysk"),
+            Printer("duża komori", "DK"),
+            Colouring(1, 0),
+            ImpositionType("f/f"),
+            size = Size(10, 10),
+            productionSize = Size(11, 11)
+        )
+        new.addWorkflowStageStop(
+            comment = null,
+            lastWorkflowStage = true,
+            assignTime = null,
+            createTime = LocalDateTime.now(),
+            worker = null,
+            workflowStage = workflowStageRepo.findById(1)!!
+        )
+        assertEquals(1, new.paperOrderTypes.size)
+        repo.save(new)
+        assertNotNull(new.orderid)
+        val found = repo.findById(2)!!
+        // checking some properties
+        assertEquals("someNameCreateTest", found.name)
+        assertEquals(100.0, found.netSize.width)
+        assertNotNull(found.comment)
+        assertEquals("julek@wp.pl", found.client.email?.email)
+        assertEquals("A1", found.bindery.name)
+        assertEquals("Jan", found.salesman.name)
+        assertEquals("Papier", found.bindingForm.name)
+
+        // size of new collections should by 1
+        assertEquals(1, found.orderEnoblings.size)
+        assertEquals(1, found.paperOrderTypes.size)
+        assertEquals(1, found.workflowStageStops.size)
+
+        // checking collections
+        val orderEnobs = found.orderEnoblings.first()
+        assertEquals("addOrderEnobling", orderEnobs.enobling.name)
+        assertEquals("A1", orderEnobs.bindery.name)
+        val papOTypes = found.paperOrderTypes.first()
+        assertEquals(10.toDouble(), papOTypes.grammage)
+        assertEquals("Papier Błysk", papOTypes.paperType.name)
+        assertEquals("duża komori", papOTypes.printer.name)
+        assertEquals(1, papOTypes.colouring.firstSide)
+        assertEquals("f/f", papOTypes.impositionType.name)
+        assertEquals(10.toDouble(), papOTypes.size.width)
+        val workflStStop = found.workflowStageStops.first()
+        assertNull(workflStStop.comment)
+        assertEquals(true, workflStStop.lastWorkflowStage)
+        assertEquals("Introligatornia", workflStStop.workflowStage.name)
     }
 }
