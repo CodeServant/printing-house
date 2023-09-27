73b59ea22b1a8cb923dc730da98bcda2c2bfefaf
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-27 13:02:47 +0200

    add serialization dependency to transport module apply kotlin serialization plugin

--- transport/build.gradle.kts
+++ transport/build.gradle.kts
@@ -1,11 +1,16 @@
 plugins {
     alias(libs.plugins.kotlin.kmm)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
     jvm {}
     sourceSets {
-        val commonMain by getting
+        val commonMain by getting {
+            dependencies {
+                implementation(libs.kotlin.serialization.json)
+            }
+        }
         val commonTest by getting
     }
 }
