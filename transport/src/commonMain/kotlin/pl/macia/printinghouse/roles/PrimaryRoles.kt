1be431d32d6ce02d138c6921fd082162e6d1ab5c
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-09-28 10:17:00 +0200

    create PrimaryRoles definitions

---
+++ transport/src/commonMain/kotlin/pl/macia/printinghouse/roles/PrimaryRoles.kt
@@ -0,0 +1,13 @@
+package pl.macia.printinghouse.roles
+
+/**
+ * [PrimaryRoles] are roles defined by the system.
+ */
+enum class PrimaryRoles {
+    ADMIN,
+    EMPLOYEE,
+    MANAGER,
+    SALESMAN,
+    OWNER,
+    WORKER
+}
