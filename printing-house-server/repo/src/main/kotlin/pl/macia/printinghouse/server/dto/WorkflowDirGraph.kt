187e6a9ca5a5eab2da590bf33f561f8d9489a5ce
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-02 11:13:36 +0200

    created WorkflowDirGraph entity

---
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/dto/WorkflowDirGraph.kt
@@ -0,0 +1,38 @@
+package pl.macia.printinghouse.server.dto
+
+import dev.drewhamilton.poko.Poko
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+import java.time.LocalDateTime
+
+@Poko
+@Entity
+@Table(name = WorkflowDirGraph.TAB_NAME)
+internal class WorkflowDirGraph(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = ID, nullable = false)
+    var id: Int?,
+    @Column(name = CREATION_TIME, nullable = false)
+    var creationTime: LocalDateTime,
+    @Column(name = COMMENT, nullable = true)
+    @NotBlank
+    @Size(max = 500)
+    var comment: String?,
+    @Column(name = NAME, nullable = false)
+    @Size(max = 300)
+    @NotBlank
+    var name: String,
+    @Column(name = CHANGED_TIME, nullable = true)
+    var changedTime: LocalDateTime?
+) {
+    companion object {
+        const val TAB_NAME = "WorkflowDirGraph"
+        const val ID = "id"
+        const val CREATION_TIME = "creationTime"
+        const val COMMENT = "comment"
+        const val NAME = "name"
+        const val CHANGED_TIME = "changedTime"
+    }
+}
