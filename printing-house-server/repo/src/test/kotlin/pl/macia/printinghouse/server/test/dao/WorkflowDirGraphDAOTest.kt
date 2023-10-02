33ddff5828c1fb51dcca38b1a1b7b719c8f0cef2
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-02 13:38:44 +0200

    add new edge check

--- printing-house-server/repo/src/test/kotlin/pl/macia/printinghouse/server/test/dao/WorkflowDirGraphDAOTest.kt
+++ printing-house-server/repo/src/test/kotlin/pl/macia/printinghouse/server/test/dao/WorkflowDirGraphDAOTest.kt
@@ -1,6 +1,8 @@
 package pl.macia.printinghouse.server.test.dao
 
-import org.junit.jupiter.api.Assertions.*
+import jakarta.transaction.Transactional
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
@@ -9,12 +11,15 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.data.repository.findByIdOrNull
 import pl.macia.printinghouse.server.PrintingHouseServerApplication
 import pl.macia.printinghouse.server.dao.WorkflowDirGraphDAO
+import pl.macia.printinghouse.server.dao.WorkflowStageDAO
 
 @SpringBootTest(classes = [PrintingHouseServerApplication::class])
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 internal class WorkflowDirGraphDAOTest {
     @Autowired
-    lateinit var dao: WorkflowDirGraphDAO
+    private lateinit var dao: WorkflowDirGraphDAO
+    @Autowired
+    private lateinit var workflowStageDAO: WorkflowStageDAO
 
     @Test
     fun findById() {
@@ -26,5 +31,16 @@ internal class WorkflowDirGraphDAOTest {
             it.id == 1
         }!!
         assertEquals("Handlowiec", edge.v1.name)
+    }
+
+    @Test
+    @Transactional
+    fun `new edge test`() {
+        var found = dao.findByIdOrNull(1)!!
+        val edge = found.addEdge(workflowStageDAO.findByIdOrNull(4)!!, workflowStageDAO.findByIdOrNull(1)!!)
+        dao.save(found)
+        found = dao.findByIdOrNull(1)!!
+        assertEquals("typical flow", found.name)
+        assertEquals(edge.v1, found.edges.find { it.id == 5 }?.v1)
     }
 }
