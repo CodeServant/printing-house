5f36099d6cd21cb87d13fcc7f23577f57a3bf2c1
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-05 16:30:52 +0200

    create constructor in WorkflowDirGraphImpl

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/WorkflowDirGraphImpl.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/WorkflowDirGraphImpl.kt
@@ -4,10 +4,38 @@ import java.time.LocalDateTime
 import pl.macia.printinghouse.server.dto.WorkflowDirGraph as PWorkflowDirGraph
 
 internal class WorkflowDirGraphImpl(p: PWorkflowDirGraph) : WorkflowDirGraph, BusinessBase<PWorkflowDirGraph>(p) {
+    constructor(
+        creationTime: LocalDateTime,
+        name: String,
+        changedTime: LocalDateTime?,
+        comment: String?
+    ) : this(
+        PWorkflowDirGraph(
+            null,
+            creationTime,
+            comment,
+            name,
+            changedTime
+        )
+    )
+
     override var wGraphId: Int? by persistent::id
     override var creationTime: LocalDateTime by persistent::creationTime
     override var comment: String? by persistent::comment
     override var name: String by persistent::name
     override var changedTime: LocalDateTime? by persistent::changedTime
     override val edge: MutableList<WorkflowDirEdge> = toBizWorkflowDirEdge(persistent.edges)
+
+fun WorkflowDirGraph(
+    creationTime: LocalDateTime,
+    name: String,
+    changedTime: LocalDateTime?,
+    comment: String?
+): WorkflowDirGraph {
+    return WorkflowDirGraphImpl(
+        creationTime,
+        name,
+        changedTime,
+        comment
+    )
 }
