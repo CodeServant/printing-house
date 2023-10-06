a36a2bf8cada8f783d0d7639ec2424a41e491ed0
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-06 16:21:54 +0200

    add test for the WorkerController

---
+++ printing-house-server/src/test/kotlin/pl/macia/printinghouse/server/test/controller/WorkerCTest.kt
@@ -0,0 +1,46 @@
+package pl.macia.printinghouse.server.test.controller
+
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.web.WebAppConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.context.WebApplicationContext
+
+/**
+ * Constants defined to reduce redundancy in tested code. Constants defined in controllers are not defined here because api may change.
+ */
+object paths {
+    const val WORKERS = "workers"
+    const val CONTEXT = "api"
+}
+
+@SpringBootTest
+@WebAppConfiguration
+internal class WorkerCTest {
+    private lateinit var mvc: MockMvc
+
+    @Autowired
+    private lateinit var webApplicationContext: WebApplicationContext
+
+    @BeforeEach
+    fun beforeEach() {
+        mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build()
+    }
+
+    @Test
+    fun `example test`() {
+        mvc.perform(MockMvcRequestBuilders.get("/${paths.CONTEXT}/${paths.WORKERS}"))
+            .andDo(::print)
+            .andExpect(status().isOk)
+            .andExpectAll(
+                jsonPath("$[1].name").value("Jiliusz"),
+                jsonPath("$[1].id").value(3),
+            )
+    }
+}
