cf744165e17c08f36c43ca7dc39ab757140620c5
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-12 13:18:29 +0200

    add UserDetailsService with users from database

---
+++ printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/config/UserDetailsService.kt
@@ -0,0 +1,34 @@
+package pl.macia.printinghouse.server.config
+
+import jakarta.transaction.Transactional
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.stereotype.Service
+import pl.macia.printinghouse.server.repository.EmployeeRepo
+
+@Service("userDetailsService")
+@Transactional
+class UserDetailsService : UserDetailsService {
+    @Autowired
+    private lateinit var employeeRepo: EmployeeRepo
+    override fun loadUserByUsername(username: String?): UserDetails {
+        if (username == null)
+            throw UsernameNotFoundException("null value passed")
+        val worker = employeeRepo.findByEmail(username)
+        if (worker == null)
+            throw UsernameNotFoundException("user not found")
+        return User(worker.email.email,
+            worker.password,
+            worker.activeAccount,
+            true,
+            true,
+            true,
+            worker.roles
+                .map { SimpleGrantedAuthority(it.name) }
+        )
+    }
+}
