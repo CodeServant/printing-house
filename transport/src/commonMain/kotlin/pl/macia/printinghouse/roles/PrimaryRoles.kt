c79e70b69f640587b8e5ab21c9384a8ed85e189c
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-12 12:15:28 +0200

    change role enums to object of constants

--- transport/src/commonMain/kotlin/pl/macia/printinghouse/roles/PrimaryRoles.kt
+++ transport/src/commonMain/kotlin/pl/macia/printinghouse/roles/PrimaryRoles.kt
@@ -3,12 +3,12 @@ package pl.macia.printinghouse.roles
 /**
  * [PrimaryRoles] are roles defined by the system.
  */
-enum class PrimaryRoles {
-    ADMIN,
-    EMPLOYEE,
-    MANAGER,
-    SALESMAN,
-    OWNER,
-    WORKER,
-    WORKFLOW_STAGE_MANAGER
+object PrimaryRoles{
+    const val ADMIN = "ADMIN"
+    const val EMPLOYEE = "EMPLOYEE"
+    const val MANAGER = "MANAGER"
+    const val SALESMAN = "SALESMAN"
+    const val OWNER = "OWNER"
+    const val WORKER = "WORKER"
+    const val WORKFLOW_STAGE_MANAGER = "WORKFLOW_STAGE_MANAGER"
 }
