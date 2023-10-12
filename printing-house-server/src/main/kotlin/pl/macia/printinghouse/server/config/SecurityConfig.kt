da6cd3837c9a29720593c221f563a47e6419cae7
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-12 11:37:32 +0200

    disable csrf in SecurityFilterChain

--- printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/config/SecurityConfig.kt
+++ printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/config/SecurityConfig.kt
@@ -2,14 +2,27 @@ package pl.macia.printinghouse.server.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf {
+                it.disable()
+            }
+            .httpBasic(Customizer.withDefaults())
+        return http.build()
+    }
+
     @Bean
     fun encoder(): PasswordEncoder {
         val encoders: MutableMap<String, PasswordEncoder> = HashMap()
