317bb4997f6475aed4d6c4e12eb4eb3dc463726b
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-13 10:42:18 +0200

    add asInt method to RecID with

--- transport/src/commonMain/kotlin/pl/macia/printinghouse/response/RecID.kt
+++ transport/src/commonMain/kotlin/pl/macia/printinghouse/response/RecID.kt
@@ -1,6 +1,7 @@
 package pl.macia.printinghouse.response
 
 import kotlinx.serialization.Serializable
+import kotlin.jvm.Throws
 
 /**
  * Record ID.
@@ -8,4 +9,17 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class RecID(
     val id: Long
-)
+) {
+    /**
+     * @throws LongToNumException if the conversion to smaller size value is impossible
+     */
+    @Throws(LongToNumException::class)
+    fun asInt(): Int {
+        if (id < Int.MIN_VALUE || id > Int.MAX_VALUE)
+            throw LongToNumException("${id::class.simpleName} should be between ${Int.MIN_VALUE} and ${Int.MAX_VALUE}")
+        return id.toInt()
+    }
+
+}
+
+class LongToNumException(str: String) : Exception(str)
