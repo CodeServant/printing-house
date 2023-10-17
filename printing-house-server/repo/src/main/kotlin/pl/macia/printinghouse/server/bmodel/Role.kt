7ad61e77d4b1c1c02330df1779cef7cb9410edf0
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-17 13:05:25 +0200

    add role annotation

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/Role.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/Role.kt
@@ -1,5 +1,8 @@
 package pl.macia.printinghouse.server.bmodel
 
+/**
+ * Roles are managed by the system and shouldn't be accessed by the users.
+ */
 sealed interface Role {
     var roleId: Int?
     var name: String
