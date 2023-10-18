57155ea4970214a7337ee557cca4742b4ffe6a79
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-10-18 11:34:33 +0200

    WorkerReq.isManagerOf defaults to empty list

--- transport/src/commonMain/kotlin/pl/macia/printinghouse/request/WorkerReq.kt
+++ transport/src/commonMain/kotlin/pl/macia/printinghouse/request/WorkerReq.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class WorkerReq(
-    val isManagerOf: List<Int>,
+    val isManagerOf: List<Int> = listOf(),
     val employed: Boolean,
     val activeAccount: Boolean,
     val password: String,
