9e86fb084563b5f26e0ce4f5208ac64524bde598
CodeServant <49171565+codeservant@users.noreply.github.com>
2023-11-06 15:21:43 +0100

    create get printer by id controller

---
+++ printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/services/PrinterService.kt
@@ -0,0 +1,27 @@
+package pl.macia.printinghouse.server.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import pl.macia.printinghouse.converting.ConversionException
+import pl.macia.printinghouse.response.PrinterResp
+import pl.macia.printinghouse.server.bmodel.Printer
+import pl.macia.printinghouse.server.repository.PrinterRepo
+
+@Service
+class PrinterService {
+    @Autowired
+    lateinit var repo: PrinterRepo
+
+    fun findById(id: Int): PrinterResp? {
+        return repo.findById(id)?.toTransport()
+    }
+}
+
+private fun Printer.toTransport(): PrinterResp {
+    return PrinterResp(
+        id = printerId
+            ?: throw ConversionException("${Printer::class.simpleName} can't be converted the ${Printer::printerId.name} is null"),
+        name = name,
+        digest = digest
+    )
+}
