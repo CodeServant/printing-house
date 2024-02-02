337d81e48bc5c942a15c3f40813459e4f20d20d3
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-02 12:10:11 +0100

    configure i18n in initial application

--- webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/cli/App.kt
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/cli/App.kt
@@ -2,10 +2,24 @@ package pl.macia.printinghouse.web.cli
 
 import io.kvision.*
 import io.kvision.html.div
+import io.kvision.i18n.DefaultI18nManager
+import io.kvision.i18n.I18n
 import io.kvision.panel.root
 
 class App : Application() {
+    init {
+        if (!(I18n.language in listOf("en", "pl"))) {
+            I18n.language = "en"
+        }
+    }
+
     override fun start() {
+        I18n.manager = DefaultI18nManager(
+            mapOf(
+                "en" to require("./i18n/messages-en.json"),
+                "pl" to require("./i18n/messages-pl.json"),
+            )
+        )
         root("kvapp") {
             div("Hello world")
             // TODO
