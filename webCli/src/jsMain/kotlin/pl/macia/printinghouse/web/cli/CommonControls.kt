c8e5cb93d7afb6c7515a69bccf233ac0687af4bf
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-02 12:04:31 +0100

    create CommonControls with standard widgets

---
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/cli/CommonControls.kt
@@ -0,0 +1,148 @@
+package pl.macia.printinghouse.web.cli
+
+import io.kvision.core.*
+import io.kvision.form.number.Numeric
+import io.kvision.form.number.numeric
+import io.kvision.form.text.Text
+import io.kvision.html.*
+import io.kvision.panel.SimplePanel
+import io.kvision.panel.hPanel
+import io.kvision.panel.simplePanel
+import io.kvision.state.ObservableValue
+import io.kvision.state.bind
+import io.kvision.tabulator.*
+
+enum class ButtonType {
+    ADD, EDIT
+}
+
+class AddButton(init: (Button.() -> Unit)? = null) : Button("dodaj", init = init) {
+    init {
+        background = Background(
+            color = Color.name(Col.FORESTGREEN)
+        )
+    }
+}
+
+class EditButton() : SimplePanel() {
+    init {
+        button("edytuj") {
+
+        }
+    }
+}
+
+class DetailsButton(text: String = "details") : Button(text = text) {
+    init {
+
+    }
+}
+
+fun Container.addButton(): AddButton {
+    return AddButton({}).apply(::add)
+}
+
+fun Container.editButton(): EditButton {
+    return EditButton().apply(::add)
+}
+
+fun Container.acceptButton() {
+    button("accept", style = ButtonStyle.SUCCESS)
+}
+
+fun Container.detailsButton() {
+    add(DetailsButton())
+}
+
+fun Container.cancelButton(onClick: () -> Unit) {
+    button("calcel", style = ButtonStyle.DANGER) {
+        onClick {
+            onClick()
+        }
+    }
+}
+
+fun Container.doubleInputField(label: String, init: (Container.() -> Unit)? = null): Numeric {
+    return numeric(label = label) {
+        init?.invoke(this)
+    }
+}
+
+class IntegerInput(label: String?, init: (IntegerInput.() -> Unit)? = null) :
+    Numeric(decimals = 0, rich = true, label = label) {
+    init {
+        init?.invoke(this)
+    }
+}
+
+/**
+ * Stabdard text input field.
+ */
+fun Container.textInput(label: String, init: (Text.() -> Unit)? = null): TextInput {
+    return TextInput(label, init = init).apply(::add)
+}
+
+class TextInput(label: String, init: (Text.() -> Unit)? = null) : Text(label = label, floating = true, init = init)
+
+/**
+ * Standard table for editing and adding elements
+ * @param onSelected what happens when specific item is selected
+ * @param formPanel is the panel that is supposed to apear in the same window as table for fast add and eddit option
+ */
+inline fun <reified T : Any> Container.insertUpdateTable(
+    summaryList: List<T>,
+    columnsDef: List<ColumnDefinition<T>>,
+    crossinline onSelected: (T?) -> Unit,
+    crossinline formPanel: () -> Component? = { null }
+) {
+    var buttonType = ObservableValue(ButtonType.ADD)
+
+    simplePanel {
+        tabulator(
+            summaryList, options = TabulatorOptions(
+                layout = Layout.FITCOLUMNS,
+                columns = columnsDef,
+                selectable = 1,
+            )
+        ) {
+            onEvent {
+                rowSelectionChangedTabulator = {
+                    buttonType.value =
+                        if (getSelectedRows().size == 1) ButtonType.EDIT else ButtonType.ADD
+                    onSelected(getSelectedData().firstOrNull())
+                }
+            }
+        }
+        hPanel(useWrappers = true).bind(buttonType) {
+            simplePanel {
+                if (it == ButtonType.ADD)
+                    addButton()
+                else {
+                    editButton()
+                }
+            }
+            formPanel()?.let(::add)
+        }
+    }
+}
+
+class LoaderSpinner(var type: SpinnerType = SpinnerType.PRIMARY) : SimplePanel() {
+    enum class SpinnerType(val cssClassName: String) {
+        PRIMARY("text-primary"),
+        SECONDARY("text-secondary"),
+        SUCCESS("text-success"),
+        DANGER("text-danger"),
+        WARRNING("text-warning"),
+        INFO("text-info"),
+        LIGHT("text-light"),
+        DARK("text-dark")
+    }
+
+    init {
+        div(className = "spinner-border ${type.cssClassName}") {
+            span(className = "visually-hidden") {
+                role = "status"
+            }
+        }
+    }
+}
