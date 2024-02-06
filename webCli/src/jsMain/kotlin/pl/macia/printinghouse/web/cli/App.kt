3712f1a9ff1099aae529c478022ca1e2063f33cb
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-06 15:17:40 +0100

    create sign in form for logging in

--- webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/cli/App.kt
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/cli/App.kt
@@ -1,10 +1,20 @@
 package pl.macia.printinghouse.web.cli
 
 import io.kvision.*
+import io.kvision.core.onClickLaunch
+import io.kvision.form.formPanel
+import io.kvision.form.text.Password
 import io.kvision.html.div
+import io.kvision.html.p
 import io.kvision.i18n.DefaultI18nManager
 import io.kvision.i18n.I18n
+import io.kvision.i18n.tr
+import io.kvision.panel.SimplePanel
 import io.kvision.panel.root
+import io.kvision.state.ObservableValue
+import io.kvision.state.bind
+import pl.macia.printinghouse.request.LoginReq
+import pl.macia.printinghouse.web.dao.LoginDao
 
 class App : Application() {
     init {
@@ -40,4 +50,38 @@ fun main() {
         TomSelectModule,
         DatetimeModule
     )
+}
+
+class SignInForm : SimplePanel() {
+    init {
+        val message = ObservableValue<String?>(null)
+        val logForm = formPanel<LoginReq> {
+            val log = textInput(tr("login"))
+            add(
+                LoginReq::login,
+                log,
+                required = true,
+                requiredMessage = tr("the field is required")
+            )
+            val pass = Password(label = tr("password"), floating = true)
+            add(
+                LoginReq::password,
+                pass,
+                required = true,
+                requiredMessage = tr("the field is required")
+            )
+        }
+        acceptButton() {
+            onClickLaunch {
+                if (logForm.validate()) {
+                    LoginDao().signIn(logForm[LoginReq::login]!!, logForm[LoginReq::password]!!) {
+                        message.value = tr("wrong credentials")
+                    }
+                }
+            }
+        }
+        p().bind(message) {
+            content = it
+        }
+    }
 }
