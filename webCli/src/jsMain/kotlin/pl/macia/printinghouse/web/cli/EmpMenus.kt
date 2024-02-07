add68efc475c0f39ef357c4096f60d52e412c8ce
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-07 15:01:46 +0100

    add BINDING_FORMS, COLOURINGS, ENOBLINGS,EMPLOYEES, PAPER_TYPES, PRINTERS, SIZES, ORKFLOW_STAGES, WORKFLOW_GRAPHS screens to manager menu

--- webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/cli/EmpMenus.kt
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/cli/EmpMenus.kt
@@ -2,27 +2,64 @@ package pl.macia.printinghouse.web.cli
 
 import io.kvision.html.button
 import io.kvision.i18n.gettext
+import io.kvision.i18n.tr
 import io.kvision.panel.SimplePanel
 import io.kvision.panel.vPanel
+import io.kvision.state.ObservableValue
+import io.kvision.state.bind
 import kotlinx.browser.document
 import kotlinx.browser.localStorage
 import pl.macia.printinghouse.roles.PrimaryRoles
 import pl.macia.printinghouse.web.StorageInfo
 
+enum class ManagerMenuScreen {
+    BINDERIES,
+    BINDING_FORMS,
+    COLOURINGS,
+    ENOBLINGS,
+    EMPLOYEES,
+    PAPER_TYPES,
+    PRINTERS,
+    SIZES,
+    WORKFLOW_STAGES,
+    WORKFLOW_GRAPHS,
+}
+
 class ManagerMenu : EmpMenu() {
     init {
         document.title = gettext("Managers Menu")
-        vPanel {
-            button("binderies")
-            button("binding forms")
-            button("colourings")
-            button("employees")
-            button("enoblings")
-            button("paper types")
-            button("printers")
-            button("size")
-            button("workflow stages")
-            button("workflow dir graph")
+        val screen = ObservableValue<ManagerMenuScreen?>(null)
+        bind(screen) {
+            when (it) {
+                null -> {
+                    fun switchScr(scr: ManagerMenuScreen) {
+                        screen.value = scr
+                    }
+                    vPanel {
+                        button(tr("binderies")) { onClick { switchScr(ManagerMenuScreen.BINDERIES) } }
+                        button(tr("binding forms")) { onClick { switchScr(ManagerMenuScreen.BINDING_FORMS) } }
+                        button(tr("colourings")) { onClick { switchScr(ManagerMenuScreen.COLOURINGS) } }
+                        button(tr("employees")) { onClick { switchScr(ManagerMenuScreen.EMPLOYEES) } }
+                        button(tr("enoblings")) { onClick { switchScr(ManagerMenuScreen.ENOBLINGS) } }
+                        button(tr("paper types")) { onClick { switchScr(ManagerMenuScreen.PAPER_TYPES) } }
+                        button(tr("printers")) { onClick { switchScr(ManagerMenuScreen.PRINTERS) } }
+                        button(tr("sizes")) { onClick { switchScr(ManagerMenuScreen.SIZES) } }
+                        button(tr("workflow stages")) { onClick { switchScr(ManagerMenuScreen.WORKFLOW_STAGES) } }
+                        button(tr("workflow graphs")) { onClick { switchScr(ManagerMenuScreen.WORKFLOW_GRAPHS) } }
+                    }
+                }
+
+                ManagerMenuScreen.BINDERIES -> add(BinderiesTab())
+                ManagerMenuScreen.BINDING_FORMS -> add(BindingFormTab())
+                ManagerMenuScreen.COLOURINGS -> add(ColouringTab())
+                ManagerMenuScreen.EMPLOYEES -> add(EmployeeTab())
+                ManagerMenuScreen.PAPER_TYPES -> add(PaperTypeTab())
+                ManagerMenuScreen.PRINTERS -> add(PrintersTab())
+                ManagerMenuScreen.SIZES -> add(SizesTab())
+                ManagerMenuScreen.WORKFLOW_STAGES -> add(WorkflowStageTab())
+                ManagerMenuScreen.WORKFLOW_GRAPHS -> add(WorkflowDirGraphTab())
+                ManagerMenuScreen.ENOBLINGS -> add(EnoblingTab())
+            }
         }
     }
 }
