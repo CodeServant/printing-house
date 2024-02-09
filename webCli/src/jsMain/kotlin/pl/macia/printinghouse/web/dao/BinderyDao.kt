5f8bb1d7d4d12beabd14b8fbe7df4d3028f30b77
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-09 10:28:55 +0100

    create rest query BinderyDao.newBinderyReq

--- webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/BinderyDao.kt
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/BinderyDao.kt
@@ -1,24 +1,45 @@
 package pl.macia.printinghouse.web.dao
 
-import io.kvision.rest.RestClient
-import io.kvision.rest.call
+import io.kvision.rest.*
 import kotlinx.browser.localStorage
+import pl.macia.printinghouse.request.BinderyReq
 import pl.macia.printinghouse.response.BinderyResp
+import pl.macia.printinghouse.response.RecID
 import pl.macia.printinghouse.web.StorageInfo
 import pl.macia.printinghouse.web.basicAuthToken
 
 class BinderyDao {
+    private val url = "http://localhost:8080/api/binderies"
+    private fun RestRequestConfig<dynamic, dynamic>.authorize() {
+        val storage = StorageInfo(localStorage)
+        headers = {
+            listOf(
+                Pair("Authorization", "Basic ${storage.basicAuthToken()}")
+            )
+        }
+    }
+
     fun allBinderies(onFulfilled: (List<BinderyResp>) -> Unit, onRejected: (Throwable) -> Unit) {
         val restClient = RestClient()
         val premise = restClient.call<List<BinderyResp>>(
-            url = "http://localhost:8080/api/binderies",
-        ){
-            val storage = StorageInfo(localStorage)
-            headers = {
-                listOf(
-                    Pair("Authorization", "Basic ${storage.basicAuthToken()}")
-                )
-            }
+            url = url,
+        ) {
+            authorize()
+        }
+        premise.then(onFulfilled, onRejected)
+    }
+
+    fun newBinderyReq(
+        binderyReq: BinderyReq,
+        onFulfilled: (RecID) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) {
+        val restClient = RestClient()
+        val premise = restClient.call<RecID, BinderyReq>(
+            url, data = binderyReq
+        ) {
+            method = HttpMethod.POST
+            authorize()
         }
         premise.then(onFulfilled, onRejected)
     }
