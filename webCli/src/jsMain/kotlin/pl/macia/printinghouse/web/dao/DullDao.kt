a57c4f14f51384a05eca09ab6ad2b3b8252e5f61
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-09 15:38:38 +0100

    add docs to DullDao

--- webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/DullDao.kt
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/DullDao.kt
@@ -4,6 +4,9 @@ import io.kvision.rest.RestClient
 import io.kvision.rest.call
 import pl.macia.printinghouse.web.authorize
 
+/**
+ * This is util dao for other dao to reduce repetitive tasks for writing simple CRUD dao.
+ */
 class DullDao(val url: String) {
     internal inline fun <reified DullResp> getAllDull(
         noinline onFulfilled: (List<DullResp>) -> Unit,
