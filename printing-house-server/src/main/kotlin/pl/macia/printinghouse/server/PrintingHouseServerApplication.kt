e0e5322b5d50501afe387d7533ca1a028c7ab205
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-12 13:32:26 +0100

    add todo for the controller

--- printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/PrintingHouseServerApplication.kt
+++ printing-house-server/src/main/kotlin/pl/macia/printinghouse/server/PrintingHouseServerApplication.kt
@@ -6,6 +6,8 @@ import org.springframework.boot.runApplication
 @SpringBootApplication
 class PrintingHouseServerApplication
 
+//todo add some controller to edit colourings
+
 fun main(args: Array<String>) {
     runApplication<PrintingHouseServerApplication>(*args)
 }
