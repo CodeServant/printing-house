10ea5541a56a60238d665e67fc610a1e89f1c121
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-12 14:46:18 +0100

    create PrinterDao

---
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/PrinterDao.kt
@@ -0,0 +1,38 @@
+package pl.macia.printinghouse.web.dao
+
+import pl.macia.printinghouse.request.PrinterChangeReq
+import pl.macia.printinghouse.request.PrinterReq
+import pl.macia.printinghouse.response.ChangeResp
+import pl.macia.printinghouse.response.PrinterResp
+import pl.macia.printinghouse.response.RecID
+
+class PrinterDao {
+    private val url = "http://localhost:8080/api/printers"
+    private val dullDao = DullDao(url)
+    fun allPrinters(onFulfilled: (List<PrinterResp>) -> Unit, onRejected: (Throwable) -> Unit) =
+        dullDao.getAllDull(onFulfilled, onRejected)
+
+    fun newPrinterReq(
+        printerReq: PrinterReq,
+        onFulfilled: (RecID) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.newDullObj(printerReq, onFulfilled, onRejected)
+
+    fun getPrinter(
+        id: Int,
+        onFulfilled: (PrinterResp) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.getOneDull(id, onFulfilled, onRejected)
+
+    fun changePrinter(
+        id: Int,
+        changeReq: PrinterChangeReq,
+        onFulfilled: (ChangeResp) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.changeDullObj(
+        id,
+        changeReq,
+        onFulfilled,
+        onRejected
+    )
+}
