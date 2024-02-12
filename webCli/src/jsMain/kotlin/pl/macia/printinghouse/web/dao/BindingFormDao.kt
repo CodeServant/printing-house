75c9584a97adec87a9a9bdf2684c796bb7d3caff
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-12 12:38:45 +0100

    create getBindingForm, insertBindingForm, delBindingForm, changeBindingForm in BindingFormDao

--- webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/BindingFormDao.kt
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/BindingFormDao.kt
@@ -1,6 +1,11 @@
 package pl.macia.printinghouse.web.dao
 
+import io.kvision.rest.RestResponse
+import pl.macia.printinghouse.request.BindingFormChangeReq
+import pl.macia.printinghouse.request.BindingFormReq
 import pl.macia.printinghouse.response.BindingFormResp
+import pl.macia.printinghouse.response.ChangeResp
+import pl.macia.printinghouse.response.RecID
 
 class BindingFormDao {
     private val url = "http://localhost:8080/api/binding-forms"
@@ -8,4 +13,28 @@ class BindingFormDao {
     fun allBindingForms(onFulfilled: (List<BindingFormResp>) -> Unit, onRejected: (Throwable) -> Unit) =
         dullDao.getAllDull(onFulfilled, onRejected)
 
+    fun getBindingForm(
+        id: Int,
+        onFulfilled: (BindingFormResp) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.getOneDull(id, onFulfilled, onRejected)
+
+    fun insertBindingForm(
+        dullObjReq: BindingFormReq,
+        onFulfilled: (RecID) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.newDullObj(dullObjReq, onFulfilled, onRejected)
+
+    fun delBindingForm(
+        id: Int,
+        onFulfilled: (RestResponse<dynamic>) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.delDullObj(id, onFulfilled, onRejected)
+
+    fun changeBindingForm(
+        id: Int,
+        changeReq: BindingFormChangeReq,
+        onFulfilled: (ChangeResp) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.changeDullObj(id, changeReq, onFulfilled, onRejected)
 }
