3c0b43c46cf01bbdbb2c8a438584bb208f6b5bd3
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-13 09:50:33 +0100

    create WorkflowStageDao newWorkflowStageReq, getWorkflowStage, delWorkflowStage, changeWorkflowStage

--- webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/WorkflowStageDao.kt
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/WorkflowStageDao.kt
@@ -1,5 +1,10 @@
 package pl.macia.printinghouse.web.dao
 
+import io.kvision.rest.RestResponse
+import pl.macia.printinghouse.request.WorkflowStageChangeReq
+import pl.macia.printinghouse.request.WorkflowStageReq
+import pl.macia.printinghouse.response.ChangeResp
+import pl.macia.printinghouse.response.RecID
 import pl.macia.printinghouse.response.WorkflowStageResp
 
 class WorkflowStageDao {
@@ -7,4 +12,34 @@ class WorkflowStageDao {
     private val dullDao = DullDao(url)
     fun allWorkflowStages(onFulfilled: (List<WorkflowStageResp>) -> Unit, onRejected: (Throwable) -> Unit) =
         dullDao.getAllDull(onFulfilled, onRejected)
+
+    fun newWorkflowStageReq(
+        workflowStageReq: WorkflowStageReq,
+        onFulfilled: (RecID) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.newDullObj(workflowStageReq, onFulfilled, onRejected)
+
+    fun getWorkflowStage(
+        id: Int,
+        onFulfilled: (WorkflowStageResp) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.getOneDull(id, onFulfilled, onRejected)
+
+    fun delWorkflowStage(
+        id: Int,
+        onFulfilled: (RestResponse<dynamic>) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.delDullObj(id, onFulfilled, onRejected)
+
+    fun changeWorkflowStage(
+        id: Int,
+        changeReq: WorkflowStageChangeReq,
+        onFulfilled: (ChangeResp) -> Unit,
+        onRejected: (Throwable) -> Unit
+    ) = dullDao.changeDullObj(
+        id,
+        changeReq,
+        onFulfilled,
+        onRejected
+    )
 }
