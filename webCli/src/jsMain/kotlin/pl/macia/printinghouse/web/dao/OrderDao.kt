f77d08f438712c92749d787089b4a4f23b1928ef
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-02-23 12:40:25 +0100

    add OrderDao.getAssigneeseOrders to fetch current orders for worker

---
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/OrderDao.kt
@@ -0,0 +1,21 @@
+package pl.macia.printinghouse.web.dao
+
+import io.kvision.rest.RestClient
+import io.kvision.rest.call
+import io.kvision.utils.obj
+import pl.macia.printinghouse.response.OrderResp
+import pl.macia.printinghouse.web.authorize
+
+class OrderDao {
+    private val url = "http://localhost:8080/api/orders"
+    fun getAssigneeseOrders(assignId: Int, onFulfilled: (List<OrderResp>) -> Unit, onRejected: (Throwable) -> Unit) {
+        val restClient = RestClient()
+        val premise = restClient.call<List<OrderResp>>(url) {
+            authorize()
+            data = obj {
+                lastAssignee = assignId
+            }
+        }
+        premise.then(onFulfilled, onRejected)
+    }
+}
