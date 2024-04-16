d16e18ed4c289da0ac51a4c526488db84b68a978
CodeServant <49171565+codeservant@users.noreply.github.com>
2024-04-16 15:27:56 +0200

    OrderDao.insertNew order

--- webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/OrderDao.kt
+++ webCli/src/jsMain/kotlin/pl/macia/printinghouse/web/dao/OrderDao.kt
@@ -3,12 +3,15 @@ package pl.macia.printinghouse.web.dao
 import io.kvision.rest.RestClient
 import io.kvision.rest.call
 import io.kvision.utils.obj
+import pl.macia.printinghouse.request.OrderReq
 import pl.macia.printinghouse.response.OrderResp
+import pl.macia.printinghouse.response.RecID
 import pl.macia.printinghouse.web.authorize
 import pl.macia.printinghouse.web.clientConfig
 
 class OrderDao {
     private val url = "${clientConfig.serviceUrl}/api/orders"
+    private val dullDao = DullDao(url)
     fun getAssigneeseOrders(onFulfilled: (List<OrderResp>) -> Unit, onRejected: (Throwable) -> Unit) {
         val restClient = RestClient()
         val premise = restClient.call<List<OrderResp>>(url) {
@@ -19,4 +22,7 @@ class OrderDao {
         }
         premise.then(onFulfilled, onRejected)
     }
+
+    fun insertNew(orderReq: OrderReq, onFulfilled: (RecID) -> Unit, onRejected: (Throwable) -> Unit) =
+        dullDao.newDullObj(orderReq, onFulfilled, onRejected)
 }
