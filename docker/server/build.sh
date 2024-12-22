ffacac3cde9798e687b5094f330412326f716230
CodeServant <49171565+CodeServant@users.noreply.github.com>
2024-12-22 21:33:41 +0100

    configure docker server file to take app version from toml file

--- docker/server/build.sh
+++ docker/server/build.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# change to project root
 cd `dirname $0`/../../
 
 ./gradlew :printing-house-server:bootJar
-docker build -t printing-house-server:0.0.1-SNAPSHOT printing-house-server/
+
+app_version_toml=`sed -n "s/^.*app\s*=\s*\x22\(\S*\)\x22.*$/\1/p" gradle/libs.versions.toml`
+
+docker build -t printing-house-server:${app_version_toml} --build-arg version=$app_version_toml printing-house-server/
+
