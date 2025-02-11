badb3bce094305dd06ccdab6f4dd136e8c01ede4
CodeServant <49171565+CodeServant@users.noreply.github.com>
2025-02-11 21:06:45 +0100

    prepare SizeImpl to change

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/SizeImpl.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/SizeImpl.kt
@@ -2,19 +2,66 @@ package pl.macia.printinghouse.server.bmodel
 
 import pl.macia.printinghouse.server.dto.Size as PSize
 
-internal class SizeImpl(persistent: PSize) : Size, BusinessBase<PSize>(persistent) {
+internal class SizeImpl private constructor(
+    var persistent: PSize?,
+    cWidth: Double?,
+    cHeight: Double?
+) : Size {
     constructor(name: String, width: Double, heigth: Double) : this(
-        PSize(name, width, heigth)
+        PSize(name, width, heigth),
+        cWidth = null,
+        cHeight = null
     )
 
     constructor(width: Double, heigth: Double) : this(
-        PSize(width, heigth)
+        null, width, heigth
     )
 
-    override var sizeId: Int? by persistent::id
-    override var name: String? by persistent::name
-    override var heigth: Double by persistent::heigth
-    override var width: Double by persistent::width
+    override var width: Double = persistent?.width ?: cWidth
+    ?: throw NullPointerException("Size implementation error, there is no ${this::sizeId.name} value")
+        get() {
+            return if (persistent != null) {
+                persistent!!.width
+            } else {
+                field
+            }
+        }
+        set(value) {
+            if (persistent != null) {
+                persistent?.width = value
+            } else {
+                field = value
+            }
+        }
+
+    override var heigth: Double = persistent?.heigth ?: cHeight
+    ?: throw NullPointerException("Size implementation error, there is no ${this::heigth.name} value")
+        get() {
+            return if (persistent != null) {
+                persistent!!.heigth
+            } else {
+                field
+            }
+        }
+        set(value) {
+            if (persistent != null) {
+                persistent?.heigth = value
+            } else {
+                field = value
+            }
+        }
+
+    override var sizeId: Int? = null
+    override var name: String?
+        get() {
+            return persistent?.name
+        }
+        set(value) {
+            if (value != null)
+                persistent?.name = value
+        }
+
+
 }
 
 fun Size(name: String, width: Number, heigth: Number): Size = SizeImpl(name, width.toDouble(), heigth.toDouble())
