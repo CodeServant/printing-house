aca8e481298706bdc08fa8a61d6463907ac652a2
CodeServant <49171565+CodeServant@users.noreply.github.com>
2025-02-12 22:07:06 +0100

    bring size rearrangement to PaperOrderTypeImpl bmodel

--- printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/PaperOrderTypeImpl.kt
+++ printing-house-server/repo/src/main/kotlin/pl/macia/printinghouse/server/bmodel/PaperOrderTypeImpl.kt
@@ -30,7 +30,11 @@ internal class PaperOrderTypeImpl(p: PPaperOrderType) : PaperOrderType, Business
             platesQuantityForPrinter,
             imposition.persistent,
             size.persistent,
+            size.width,
+            size.heigth,
             productionSize.persistent,
+            productionSize.width,
+            productionSize.heigth,
             order.persistent
         )
     )
@@ -51,8 +55,44 @@ internal class PaperOrderTypeImpl(p: PPaperOrderType) : PaperOrderType, Business
         ImpositionType::class.java
     )
     override val order: Order by delegate(persistent::order, ::OrderImpl, Order::class.java)
-    override var size: Size by delegate(persistent::size, ::SizeImpl, Size::class.java)
-    override var productionSize: Size by delegate(persistent::productionSize, ::SizeImpl, Size::class.java)
+    override var size: Size
+        get() = if (persistent.size != null) SizeImpl(persistent.size!!) else SizeImpl(
+            persistent.sizeWidth!!,
+            persistent.sizeHeight!!
+        )
+        set(value) {
+            fun setNew(newSize: SizeImpl) {
+                persistent.size = newSize.persistent
+                persistent.sizeWidth = newSize.width
+                persistent.sizeHeight = newSize.heigth
+            }
+            if (value.sizeId == null && value.name == null) {
+                setNew(SizeImpl(value.width, value.heigth))
+            } else if (value.sizeId == null && value.name != null) {
+                setNew(SizeImpl(value.name!!, value.width, value.heigth))
+            } else if (value.sizeId != null && value.name != null) {
+                setNew(value as SizeImpl)
+            }
+        }
+    override var productionSize: Size
+        get() = if (persistent.productionSize != null) SizeImpl(persistent.productionSize!!) else SizeImpl(
+            persistent.productionSizeWidth!!,
+            persistent.productionSizeHeight!!
+        )
+        set(value) {
+            fun setNew(newSize: SizeImpl) {
+                persistent.productionSize = newSize.persistent
+                persistent.productionSizeWidth = newSize.width
+                persistent.productionSizeHeight = newSize.heigth
+            }
+            if (value.sizeId == null && value.name == null) {
+                setNew(SizeImpl(value.width, value.heigth))
+            } else if (value.sizeId == null && value.name != null) {
+                setNew(SizeImpl(value.name!!, value.width, value.heigth))
+            } else if (value.sizeId != null && value.name != null) {
+                setNew(value as SizeImpl)
+            }
+        }
 
 }
 
